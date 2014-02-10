969d3587110ce92d217b6d395ff4f625abe7c462
Boris Kozorovitzky <majorzbzzn@gmail.com>
2014-02-10 17:11:12 +0200

    Added TransactionWriterAddin

---
+++ Experimental/TransactionWriterAddin/AutostartCommand.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------
+// © Copyright 2013 Hewlett-Packard Development Company, L.P.
+//--------------------------------------------------------------------
+
+using HP.LR.Vugen.Common;
+using HP.LR.VuGen.ServiceCore;
+using HP.LR.VuGen.ServiceCore.Data.ProjectSystem;
+using HP.LR.VuGen.ServiceCore.Interfaces;
+using HP.Utt.UttCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionWriterAddin
+{
+  public class AutostartCommand : UttBaseCommand
+  {
+
+    public override void Run()
+    {
+      VuGenServiceManager.RunWhenInitialized(InnerRun);
+    }
+
+    private void InnerRun()
+    {
+      IVuGenProjectService projectService = VuGenServiceManager.GetService<IVuGenProjectService>();
+      projectService.ScriptSaved += projectService_ScriptSaved;
+    }
+
+    void projectService_ScriptSaved(object sender, HP.Utt.ProjectSystem.SaveEventArgs e)
+    {
+      try
+      {
+
+        IVuGenScript script = e.Script as IVuGenScript;
+        if (script == null) return;
+        List<IExtraFileScriptItem> extraFiles = script.GetExtraFiles();
+        IExtraFileScriptItem dynamicTransacrions = null;
+        foreach (IExtraFileScriptItem extraFile in extraFiles)
+        {
+          if (extraFile.FileName == "dynamic_transactions.txt") //This is not configurable. This is the file name!
+          {
+            dynamicTransacrions = extraFile;
+            break;
+          }
+        }
+
+        if (dynamicTransacrions == null) return;
+
+        string fileContent = File.ReadAllText(dynamicTransacrions.FullFileName);
+        string[] transactionNames = fileContent.Split(new String[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        IScriptDataObject scriptData = script.GenerateDataObject() as IScriptDataObject;
+        if (scriptData == null) return;
+        List<String> transactionsList = scriptData.SpecialSteps.Transactions;
+        transactionsList.AddRange(transactionNames);
+        IniUsrFile usr = new IniUsrFile(script.FileName);
+        usr.WriteTransactions(transactionsList);
+        usr.Save();
+      } 
+      catch
+      {
+        //Don't want to have adverse influence on the regular usage of VuGen. If this fails it does so silently. 
+      }
+
+
+    }
+  }
+}
---
+++ Experimental/TransactionWriterAddin/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------
+// © Copyright 2013 Hewlett-Packard Development Company, L.P.
+//--------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TransactionWriterAddin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("TransactionWriterAddin")]
+[assembly: AssemblyCopyright("© Copyright 2013 Hewlett-Packard Development Company, L.P.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed8db54e-8124-4b74-9f5c-9e8e1b47c681")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
